4a0c02f125430f41d62f08c655e7eb539fc240bc
Hakan <52114496+celikdev@users.noreply.github.com>
2024-10-29 01:06:01 +0300

    fix url

--- src/app/components/provider.tsx
+++ src/app/components/provider.tsx
@@ -16,8 +16,8 @@ export default function Procider({ children }: { children: React.ReactNode }) {
   // const url = "http://localhost:3000/api/trpc";
 
   const url = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    : "http://localhost:3000/api/trpc";
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}/api/trpc`
+    : "http://localhost:3000/api/trpc/";
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
